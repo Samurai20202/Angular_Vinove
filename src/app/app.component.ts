18d0384d5531eb7ef46d4a7b852ff7bf61bbff6c
Utkarsh Gupta <utkarshgupta726@outlook.com>
2021-06-14 22:01:27 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormControl, Validators} from '@angular/forms';
+  
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'POCC';
+  websiteList: any = ['Birth Certificate', 'Previous Class Marksheet', 'Aadhaar Card','Conduct Certicate','Transfer Certificate']
+  FileList: any= ['.pdf','.jpg/.jpeg','.rar','.zip']
+  form = new FormGroup({
+    website: new FormControl('', Validators.required),
+    filelist: new FormControl('', Validators.required)
+  });
+  ngOnInit(){
+    this.dataArray.push(this.home);
+
+  }
+
+  get f(){
+    return this.form.controls;
+  }
+
+  submit(){
+    console.log(this.dataArray);
+  }
+
+  
+  addRow(){
+    
+    this.home=new home();
+    this.dataArray.push(this.home);
+  }
+
+  remove(index:any){
+    this.dataArray.splice(index,1);
+
+  }
 }
